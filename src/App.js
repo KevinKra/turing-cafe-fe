11ba979e3f624605ff3f6c19c79d0247f2343bea
Kevin Krato <kratok08@gmail.com>
2019-05-16 09:56:05 -0600

    Collect inputs from FormBar component and store in App state array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import FormBar from "../src/components/FormBar/FormBar";
+import CardSection from "../src/components/CardSection/CardSection";
 
 class App extends Component {
+  state = {
+    reservations: []
+  };
+  makeReservation = (state, e) => {
+    e.preventDefault();
+    const newState = state;
+    const combinedState = [...this.state.reservations, newState];
+    this.setState({ reservations: combinedState });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-          
-        </div>
+        <header>Title</header>
+        <FormBar makeReservation={this.makeReservation} />
+        <CardSection />
       </div>
-    )
+    );
   }
 }
 
