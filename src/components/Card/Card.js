139292f613329a82b63ef019aa3a6c19cd231ad0
Kevin Krato <kratok08@gmail.com>
2019-05-16 09:55:33 -0600

    Create components folder

---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="Card">
+        <section>Card</section>
+      </div>
+    );
+  }
+}
+
+export default Card;
---
+++ src/components/CardSection/CardSection.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Card from "../Card/Card";
+
+class CardSection extends Component {
+  render() {
+    return (
+      <div className="CardSection">
+        <section>CardSection</section>
+        <Card />
+      </div>
+    );
+  }
+}
+
+export default CardSection;
