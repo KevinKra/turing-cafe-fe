891e8effcf35ce58d33940683e4e4c0bb8d626e0
Kevin Krato <kratok08@gmail.com>
2019-05-16 10:25:55 -0600

    Render card with passed data

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import "./Card.css";
 
 class Card extends Component {
   render() {
+    const { name, date, time, guests, id } = this.props;
     return (
       <div className="Card">
-        <section>Card</section>
+        <section>
+          <h3>{name}</h3>
+          <p>{date}</p>
+          <p>{time} pm</p>
+          <p>Number of Guests: {guests}</p>
+        </section>
+        <button
+          className="btn"
+          onClick={() => this.props.removeReservation(id)}
+        >
+          Cancel
+        </button>
       </div>
     );
   }
